b72446224a9bf32f32893326615571560364b86e
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-22 20:05:18 -0700

    Added link to routing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,15 +4,13 @@ import HomeComponent from './HomeComponent';
 import AboutComponent from './AboutComponent';
 import ContactComponent from './ContactComponent';
 import ProjectsComponent from './ProjectsComponent';
-import ResumeComponent from './ResumeComponent';
 
 const MainComponent = () => (
     <Switch>
         <Route exact path = "/" component = {HomeComponent} />
-        <Route exact path = "/" component = {AboutComponent} />
-        <Route exact path = "/" component = {ContactComponent} />
-        <Route exact path = "/" component = {ProjectsComponent} />
-        <Route exact path = "/" component = {ResumeComponent} />
+        <Route path = "/about" component = {AboutComponent} />
+        <Route path = "/contact" component = {ContactComponent} />
+        <Route path = "/projects" component = {ProjectsComponent} />
 
     </Switch>
 )

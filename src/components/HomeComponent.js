5f35af865758d4f3332203e159d0911c449f5060
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-24 13:56:25 -0700

    Added avatar and some text to Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -5,8 +5,17 @@ export class Home extends Component {
   render() {
     return (
       <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-          <Cell col={12}></Cell>
+        <Grid className="home-grid">
+          <Cell col={12}>
+            <img 
+              src="https://images.vexels.com/media/users/3/200291/isolated/preview/4b49ff1bd841b34def1c19c783a262fb-anonymous-avatar-woman-long-wild-hair-by-vexels.png"
+              alt="Avatar"
+              className="avatar"
+            />
+            <div className="banner-text">
+              <h1>Graphic designer</h1>
+            </div>
+          </Cell>
 
         </Grid>
       </div>

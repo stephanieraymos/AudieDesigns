85a7b96129f4db7dceb9689bc4df02b41b8b4695
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-24 18:26:15 -0700

    Added card to Adobe Project

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 export class Projects extends Component {
 
@@ -14,9 +14,18 @@ export class Projects extends Component {
 
     if (this.state.activeTab === 0) {
       return (
-        <div>
-          <h1>This is Adobe Illustrator and Adobe Photoshop</h1>
-        </div>
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: 'black', height: '176px', background: 'url("https://snobmonkey.com/wp-content/uploads/2019/04/adobe-blog-banner-1-1024x585-1024x585.jpg") center / cover'}}>Adobe #1</CardTitle>
+          <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </CardText>
+          <CardActions border>
+            <Button colored>Link to all images</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
       )
     }else if (this.state.activeTab === 1) {
       return (
@@ -39,7 +48,13 @@ export class Projects extends Component {
         </Tabs>
 
         <section className="projects-grid">
-          {this.toggleCatergories()}
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">          
+                {this.toggleCatergories()}
+              </div>
+            </Cell>
+          </Grid>
         </section>
       </div>
     )

51c7cf20eace256be3643959aeb8b5380f646469
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-25 12:53:21 -0700

    Fixed Contact, made it responsive with react-bootstrap

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { Grid, Cell, List, ListItem, ListItemContent, Button } from 'react-mdl';
+import { List, ListItem, ListItemContent } from 'react-mdl';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 class Contact extends Component {
   render() {
     return (
     
       <div className="contact-body">
-        <Grid className="contact-grid">
-          <Cell col={6}>
+        <Container className="contact-grid">
+          <Row>
+            <Col>
             <h2>Audie Raymos</h2>
             <img
               src="https://cdn0.iconfinder.com/data/icons/avatars-3/512/avatar_beanie_girl-512.png"
@@ -15,8 +18,8 @@ class Contact extends Component {
               style={{height: '250px'}}
             />
             <p style={{width: '70%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </Cell>
-          <Cell col={6}>
+          </Col>
+          <Col>
             <h2>Contact Me</h2>
             <hr/>
 
@@ -37,9 +40,9 @@ class Contact extends Component {
                 </ListItem>
               </List>
             </div>
-
-          </Cell>
-        </Grid>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }

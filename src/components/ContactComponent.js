cdba293ee773b5bbf0870eeffa74dbb2272f71ff
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-14 13:06:09 -0700

    Added avatar + mailto to contact

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { List, ListItem, ListItemContent } from 'react-mdl';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import audie from './img/audie.jpg'
 
 class Contact extends Component {
   render() {
@@ -13,10 +13,10 @@ class Contact extends Component {
             <Col>
             <h2>Audie Raymos</h2>
             <img
-              src="https://cdn0.iconfinder.com/data/icons/avatars-3/512/avatar_beanie_girl-512.png"
-              alt="avatar"
-              style={{height: '250px'}}
-            />
+                src={audie}
+                alt="avatar"
+                style={{ height: '300px', width: '225px', objectFit: 'contain' }}
+              />
             <p style={{width: '70%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </Col>
           <Col>
@@ -27,8 +27,8 @@ class Contact extends Component {
               <List>
                 <ListItem>
                   <ListItemContent style={{ fontSize: '30px', fontFamily: 'Oswald'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    audriannakraymos@gmail.com
+                  <a class="mailto" style={{color: 'black'}} href="mailto:audriannakraymos@gmail.com"><i className="fa fa-envelope" aria-hidden="true" />
+                    audriannakraymos@gmail.com</a>
                   </ListItemContent>
                 </ListItem>
 
